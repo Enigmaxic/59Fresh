49db5bacdd124f65d2f3d6d6ea0352917029130c
Enigmaxic <smittanator@gmail.com>
2015-03-02 01:41:03 -0500

    Mobilized entrepreneurSignup
    Made entrepreneurSignup mobile.  Hitting submit reloads elements on top
    of elements already there instead of displaying errors.  Login page also
    has a similar problem

--- entrepreneurSignup.php
+++ entrepreneurSignup.php
@@ -12,13 +12,11 @@ session_start();
     <head>
         <meta charset="UTF-8">
         <title>59SecondPitch</title>
-        <link href="CSS/bootstrap.min.css" rel="stylesheet" media="screen">
-        <link href="CSS/newProfileStyles.css" rel="stylesheet">
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
     </head>
     <body>
         <script src="http://code.jquery.com/jquery.js"></script>
-        <script src="JS/bootstrap.min.js"></script>
-        <script src="JS/myScript.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
         <?php
         $workType = $workName = $workDesc = $business_video = "";
@@ -60,23 +58,30 @@ session_start();
         <div class="container">
             <!--form setup taken from tutorial for twitter bootstrap-->
             <form id="entrepreneurForm" class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
-                <label>How would you classify your work?</label><br>
-                <input id="ideaRadio" type="radio" name="workType" value="idea" checked="<?php if ($_POST['business_type'] == "idea"){echo "true";}  ?>">Idea<span class="error">* <?php echo $workTypeerr; ?></span><br>
-                <input id="projectRadio" type="radio" name = "workType" value="project" checked="<?php if ($_POST['business_type'] == "project"){echo "true";}  ?>">Project<br>
-                <input id="startupRadio" type="radio" name="workType" value="startup" checked="<?php if ($_POST['business_type'] == "startup"){echo "true";}  ?>">Startup<br>
-                <input id="companyRadio" type="radio" name="workType" value="company" checked="<?php if ($_POST['business_type'] == "company"){echo "true";}  ?>">Company<br>
-                
-                <br><label>What is the name of your work?</label>
+                <label for="businessType" class="select">Classification:</label>
+                <select name="businessType" id="businessType">
+                    <option value="Company">Company</option>
+                    <option value="Startup">Startup</option>
+                    <option value="Project">Project</option>
+                    <option value="Idea">Idea</option>
+                </select>
+
+                <label for="workName">Venture Name:</label>
                 <input type="text" id="workName" name="workName" value = "<?php
                 if (isset($_POST['business_name'])) {
                     echo $_POST['business_name'];
                 } else if (isset($_POST['workName'])) {
                     echo $_POST['workName'];
                 }
-                ?>"> <span class="error">* <?php echo $workNameerr; ?></span><br>
-                <br><label>Pitch Video Vimeo ID:</label>
-                <input type="text" id="business_video" name="business_video" value ="<?php  if (isset($_POST['business_video'])){echo $_POST['business_video'];}; ?>"> <span class="error">* <?php echo $businessVideoerr; ?></span><br>
-                <br><button type="submit" class="btn">Submit</button>
+        ?>"> 
+                <span class="error"><?php if (!empty($workNameerr)) {echo "*" . $workNameerr;} ?>
+                    
+                <label for="businessVideo">Pitch Video Vimeo ID:</label>
+                <input type="text" id="businessVideo" name="businessVideo" value = "<?php if (isset($_POST['business_video'])) {
+                    echo $_POST['business_video'];
+                }; ?>"> 
+                <span class="error"><?php if (!empty($businessVideoerr)) {echo "*" . $businessVideoerr;} ?>
+                <button type="submit" class="btn">Submit</button>
             </form>
         </div>
     </body>
