839bf592077b1f52106295e19f55b6e1014b2298
Maxwell Smith <smittanator@gmail.com>
2015-03-23 14:09:29 -0400

    Added lots of debugging code to login
    Still cant fix file upload

--- JS/login.js
+++ JS/login.js
@@ -11,20 +11,20 @@ OAuth 1.0a User Secret:
 7e960f43-1ad6-42a4-92ce-f04c4dacd60d
 Don't share this secret with anyone.
 */
-$(document).on('change', '.btn-file :file', function() {
+/*$(document).on('change', '.btn-file :file', function() {
     var input = $(this),
             numFiles = input.get(0).files ? input.get(0).files.length : 1,
             label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
     input.trigger('fileselect', [numFiles, label]);
-});
+});*/
 
 $(document).ready(function() {
-    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+    /*$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
         if (numFiles > 1) {
             alert("Please select only 1 picture");
         }
         $("#profilePictureURL").val(label);
-    });
+    });*/
     /* $.ajax({
         type: "POST",
         dataType: "json",
