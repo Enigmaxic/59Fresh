9ec17ca6a10d53b6c8dce0a82e4e3f6459186fad
Enigmaxic <smittanator@gmail.com>
2015-04-06 14:35:42 -0400

    Revert "Feedback page started"
    This reverts commit 3da549aed955648fd3152c6fcd3ac6f32038fc0a.

--- JS/entrepreneurStatistics.js
+++ JS/entrepreneurStatistics.js
@@ -1,7 +1,8 @@
 
 function displayStatistics(data) {
     var profile;
-    
+    var feedback1="";
+    var feedback2="";
     
     for (i = 0; i < Object.keys(data).length; i++) {
         profile = data["" + i];
@@ -33,16 +34,7 @@ function displayStatistics(data) {
                 '<p><span style="color:green; font-weight:bold;">Matches:   </span>'+ profile[1]+'</p>'+
                 "<p><span style='color:red; font-weight:bold;'>No's:   </span>" + profile[2] + '</p><br><br>' +
                 '</div>').collapsibleset('refresh');
-    
-    /*$("#feedbackSet").append('<div data-role="collapsible" >'+
-            '<h3>'+profile[0]+" Feedback"+'</h3>'+
-            
-                '<p><span style="color:white;">'+ profile[3]+'</p>'+
-                '<p><span style="color:white;">' + profile[4] + '</p><br><br>' +
-                '</div>').collapsibleset('refresh');*/
     }
-    
-
 }
  /*$("body").append("<div class='panel panel-default businessCard'>" +
                 "<div class='panel-heading'>" +
--- entrepreneurStatistics.php
+++ entrepreneurStatistics.php
@@ -28,37 +28,7 @@
             </div><!-- /header -->
 
             <div data-role="content" id="stats" style="background:'#000000'">	
-                <a href="#feedback" rel="external" data-role="button">View Feedback</a>
                 <div data-role="collapsible-set" data-theme="b" data-content-theme="b" id="set"></div>
-            </div>
-
-            <div data-role="footer">
-                <img id="logo" src="IMG/59SecondPitchLogo.png" style="display:block;margin:1em auto 0;width:75%">
-            </div>
-        </div>
-        
-        
-        <div id ="feedback" data-role="page" data-theme="b">
-            
-
-            <div data-role="header">
-                <a href="/59SecondPitch/entrepreneurHome.php" rel="external" data-icon="home">Home</a>
-                <h1>Feedback</h1>					
-                <a data-role="none">
-                    <label for="navMenu" class="ui-hidden-accessible">Menu</label>
-                    <select name="navMenu" id="navMenu" data-icon="bars">
-                        <option selected disabled>Menu</option>
-                        <option value="/59SecondPitch/manageProfile.php">Manage Profile</option>
-                        <option value="/59SecondPitch/entrepreneurIdeas.php">My Ventures</option>
-                        <option value="/59SecondPitch/entrepreneurMatches.php">Matches</option>
-                        <option value="/59SecondPitch/entrepreneurStatistics.php">Statistics</option>
-                        <option value="/59SecondPitch/login.php">Logout</option>
-                    </select>
-                </a>
-            </div><!-- /header -->
-
-            <div data-role="content" id="feedback" style="background:'#000000'">	
-                <div data-role="collapsible-set" data-theme="b" data-content-theme="b" id="feedbackSet"></div>
             </div>
 
             <div data-role="footer">
