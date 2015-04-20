fe32fa21c3a8540f3ddf0b554c40aea9564adde4
Maxwell Smith <smittanator@gmail.com>
2015-04-20 13:56:48 -0400

    swipe feature and display added

--- JS/BrowseMobile.js
+++ JS/BrowseMobile.js
@@ -7,37 +7,62 @@
 function displayNewProfile(data) {
     var profile;
     //alert(JSON.stringify(data));
-    $("#profile").append('<div class ="cover " name ="" id ="1" style ="border: 2px solid;"></div>');
-
     profile = data[0];
-    $("#1").html(
-        '<div>' +
-            '<div>' +
-                '<h2><font color="white">' + profile["firstname"] + " " + profile["lastname"] + '</font></h2>' +
-                '<p><strong><font color="red">Alma mater: </font></strong><font color="white">' + profile['almamater'] + '</font></p>' +
-                '<p><strong><font color="red">City: </font></strong><font color="white">' + profile['city'] + '</font></p>' +
-            '</div>' +              
-            '<div class="text-center">' +
-                '<figure><img src="PROFILE_PICTURES/' + profile['profilepicture'] + '" alt="Profile Picture missing" class="profilepicture"></figure>' +
-            '</div>' +
-        '</div>' +
-        '<div class="">' + 
-            '<iframe width="500" height="275" src="' + profile['business_video'] + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>' + 
-        '</div>');
-    $("#" + i).attr("name",profile['business_id']);     
+    $("#profileInfo").html(
+            '<h2><font color="white">' + profile["firstname"] + " " + profile["lastname"] + '</font></h2>' +
+            '<p><strong><font color="red">Alma mater: </font></strong><font color="white">' + profile['almamater'] + '</font></p>' +
+            '<p><strong><font color="red">City: </font></strong><font color="white">' + profile['city'] + '</font></p>');
+    
+    $("#profilePicture").html('<figure><img id="profilepicture" src="PROFILE_PICTURES/' + profile['profilepicture'] + '" alt="Profile Picture missing" class="profilepicture"></figure>');
+    
+    $("#profileVideo").html('<iframe src="' + profile['business_video'] + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+    
+    $("#1").attr("name", profile['business_id']);
 }
 
+function swipeleftHandler(event){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        async: false,
+        url: "fetchProfile.php",
+        //Set cover content to the 5 fetched profiles
+        success: function(data) {
+            displayNewProfile(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Error: " + jqXHR.responseText);
+        }
+    });
+}
+function swiperightHandler(event){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        async: false,
+        url: "fetchProfile.php",
+        //Set cover content to the 5 fetched profiles
+        success: function(data) {
+            displayNewProfile(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Error: " + jqXHR.responseText);
+        }
+    });
+}
 function ShowDialog(modal)
-{    
-   $("#dialog").fadeIn(300);
+{
+    $("#dialog").fadeIn(300);
 }
 
 function HideDialog()
-{  
-   $("#dialog").fadeOut(300);
-} 
+{
+    $("#dialog").fadeOut(300);
+}
 
 $(document).ready(function() {
+    $(".cover").on("swipeleft",swipeleftHandler);
+    $(".cover").on("swiperight",swiperightHandler);
     //Grab 5 profiles to start
     $.ajax({
         type: "POST",
@@ -52,101 +77,109 @@ $(document).ready(function() {
             alert("Error: " + jqXHR.responseText);
         }
     });
-    
-    $("#btnClose").click(function(){
+
+    $("#btnClose").click(function() {
         HideDialog();
     });
-    $("#btnSubmit").click(function(){
+    $("#btnSubmit").click(function() {
         var id = $('.coverflow').coverflow("cover").attr('name');
         var radio;
-        if($('#feed1').is(':checked')) { radio = "Not interested in product."; }
-        if($('#feed2').is(':checked')) { radio = "Unrealistic"; }
-        if($('#feed3').is(':checked')) { radio = "Needs Refinement"; }
-        
-        if($("#feed4").val()!== ""){
-            $.post('feedback.php', { 
+        if ($('#feed1').is(':checked')) {
+            radio = "Not interested in product.";
+        }
+        if ($('#feed2').is(':checked')) {
+            radio = "Unrealistic";
+        }
+        if ($('#feed3').is(':checked')) {
+            radio = "Needs Refinement";
+        }
+
+        if ($("#feed4").val() !== "") {
+            $.post('feedback.php', {
                 businessid: id,
                 radio: radio,
-                other:$("#feed4").val()},
-                function(data){}).fail(function() {
-                    alert( "Posting failed." );
-                });
+                other: $("#feed4").val()},
+            function(data) {
+            }).fail(function() {
+                alert("Posting failed.");
+            });
         }
-        else{
-            $.post('feedback.php', { 
+        else {
+            $.post('feedback.php', {
                 businessid: id,
                 radio: radio},
-                function(data){}).fail(function() {
-                    alert( "Posting failed." );  
-                });
+            function(data) {
+            }).fail(function() {
+                alert("Posting failed.");
+            });
         }
         HideDialog();
     });
-    
-    $(document).on('click','.viewProfile',function() {
+
+    $(document).on('click', '.viewProfile', function() {
         var id = $('.coverflow').coverflow("cover").attr('id');
         $('#profiles').hide();
-        $('#fullProfile').css("display","block");
-        $('#f'+id).css("display","block");
-        
-        
+        $('#fullProfile').css("display", "block");
+        $('#f' + id).css("display", "block");
+
+
     });
-    $(document).on('click','.return',function(event){
+    $(document).on('click', '.return', function(event) {
         var id = $('.coverflow').coverflow("cover").attr('id');
-        $('#f'+id).css("display","none");
-        $('#fullProfile').css("display","none");
+        $('#f' + id).css("display", "none");
+        $('#fullProfile').css("display", "none");
         $('#profiles').show();
     });
-    $(document).on('click','.match',function() {
+    $(document).on('click', '.match', function() {
         var id = $('.coverflow').coverflow("cover").attr('name');
         //var arr = {"businessid":};
-        $.post('browseMatching.php', { businessid: id,match: 1}, function(data){
-             
-           $("#"+$('.coverflow').coverflow("cover").attr('id')).html("<img src = '"+"IMG/check.png' />");
-            
-             
-        }).fail(function() {
-         
-            
-            alert( "Posting failed." );
-             
-        });
-       
-    });
-       
+        $.post('browseMatching.php', {businessid: id, match: 1}, function(data) {
 
-    $(document).on('click','.nothanks',function (){
+            $("#" + $('.coverflow').coverflow("cover").attr('id')).html("<img src = '" + "IMG/check.png' />");
+
+
+        }).fail(function() {
+
+
+            alert("Posting failed.");
+
+        });
+
+    });
+
+
+    $(document).on('click', '.nothanks', function() {
         var id = $('.coverflow').coverflow("cover").attr('name');
         //var arr = {"businessid":};
-        
-        $.post('browseMatching.php', { businessid: id,match: 0}, function(data){
-             
-           $("#"+$('.coverflow').coverflow("cover").attr('id')).html("<img src = '"+"IMG/x.jpeg' />");
+
+        $.post('browseMatching.php', {businessid: id, match: 0}, function(data) {
+
+            $("#" + $('.coverflow').coverflow("cover").attr('id')).html("<img src = '" + "IMG/x.jpeg' />");
             ShowDialog();
-             
+
         }).fail(function() {
-         
-            
-            alert( "Posting failed." );
-             
+
+
+            alert("Posting failed.");
+
         });
     });
-    $(document).on('click','.maybe',function (){
+    $(document).on('click', '.maybe', function() {
         var id = $('.coverflow').coverflow("cover").attr('name');
         //var arr = {"businessid":};
-        $.post('browseTracking.php', { businessid: id}, function(data){
-             
-           
+        $.post('browseTracking.php', {businessid: id}, function(data) {
+
+
             alert("You have begun tracking this profile!");
-             
+
         }).fail(function() {
-         
-            
-            alert( "Posting failed." );
-             
+
+
+            alert("Posting failed.");
+
         });
     });
-    
+
 });
 
 
--- browse.php
+++ browse.php
@@ -11,85 +11,61 @@ include_once 'FiftyNineDAO.php';
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Profile Browser</title>
     </head>
-    <div id ="browsePage" data-role="page" >
-        <link href="CSS/bootstrap.min.css" rel="stylesheet" media="screen">
-        <link rel="stylesheet" type="text/css" href="CSS/browseStyles.css" />      
-        <link rel="stylesheet" type="text/css" href="CSS/feedback.css" />
-        <link rel="stylesheet" href="themes/59Th.min.css" />
-        <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+    <body>
+        <div id ="browsePage" data-role="page" >     
 
-        <script src="http://code.jquery.com/jquery.js"></script>
-        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-        <script src="JS/bootstrap.min.js"></script>
-        <script src="JS/jquery.coverflow.js"></script>
-        <script src="JS/browseMobile.js"></script>
+            <link rel="stylesheet" href="themes/59Th.min.css" />
+            <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+            <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+            <link rel="stylesheet" type="text/css" href="CSS/browseStyles.css" />  
 
-        <div data-role="header">
-            <a href="/59SecondPitch/investorHome.php" rel="external" data-icon="home">Home</a>
-            <h1>Home</h1>
-            <div data-role="navbar">
-                <ul>
-                    <li><a href="/59SecondPitch/manageProfile.php" rel='external'>Manage Profile</a></li>
-                    <li><a href="/59SecondPitch/browse.php" rel='external'>Browse Ventures</a></li>
-                    <li><a href="/59SecondPitch/investorFavorites.php" rel='external'>Favorites</a></li>
-                    <li><a href="/59SecondPitch/investorTracking.php" rel='external'>Tracking</a></li>
-                    <li><a href="/59SecondPitch/login.php" rel='external'>Logout</a></li>
-                </ul>
-            </div><!-- /navbar -->
-        </div>
-        <div data-role="content">
-            <!--Profile Container-->
-            <div id="profileContainer" class="container-fluid1">
-                <img id="browseBackground" src="IMG/city3.jpg">
-                <div  id = "profiles"class="row-fluid">
-                    <div id ="profile" class ="coverflow  text-center"   >
+            <script src="http://code.jquery.com/jquery.js"></script>
+            <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+            <script src="JS/browseMobile.js"></script>
 
+            <div data-role="header">
+                <a href="/59SecondPitch/investorHome.php" rel="external" data-icon="home">Home</a>
+                <h1>Home</h1>
+                <div data-role="navbar">
+                    <ul>
+                        <li><a href="/59SecondPitch/manageProfile.php" rel='external'>Manage Profile</a></li>
+                        <li><a href="/59SecondPitch/browse.php" rel='external'>Browse Ventures</a></li>
+                        <li><a href="/59SecondPitch/investorFavorites.php" rel='external'>Favorites</a></li>
+                        <li><a href="/59SecondPitch/investorTracking.php" rel='external'>Tracking</a></li>
+                        <li><a href="/59SecondPitch/login.php" rel='external'>Logout</a></li>
+                    </ul>
+                </div><!-- /navbar -->
+            </div>
+            <div data-role="content">
+                <div class = "cover" id ="1">
+                    <div class = "ui-grid-a" >
+                        <div class="ui-block-a" id="profileInfo">
+                        </div>
+                        <div class="ui-block-b" id="profilePicture">
+                        </div>
                     </div>
-                </div>
-                <div id ="fullProfile" class ="row-fluid" style="display: none;">
-                    <div id ="fullProfileDisplay" class ="text-center">
-                        <div id ="f1" style ="display: none;" ></div>
+                    <div class = "ui-grid-solo" id="profileVideo">
                     </div>
                 </div>
             </div>
-            
-            <div id="dialog" class="web_dialog">
-                <table style="width: 100%; border: 0px;" cellpadding="3" cellspacing="0">
-                    <tr>
-                        <td class="web_dialog_title">Feedback</td>
-                        <td class="web_dialog_title align_right">
-                            <a href="#" id="btnClose">Close</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" style="padding-left: 15px;">
-                            <b>Entrepreneurs appreciate your feedback. </b>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" style="padding-left: 15px;">
-                            <form>
-                                <input id="feed1"  type="radio" name ="feedback"  value="interest" checked/> Not interested in product<br>
-                                <input id="feed2" type="radio" name ="feedback"  value="realistic" /> Unrealistic <br>
-                                <input id="feed3"  type="radio" name ="feedback"   value="refine" /> Needs refinement<br>
-                                <input id ="feed4" value = ""  name ="feedback" type="text" /> Other<br>
-                                <input id="btnSubmit" style ="text-align: center"type="button"  value="Submit" />
-                            </form>
-                        </td>
-                    </tr>
-                    <tr><td colspan="2" style="text-align: center;"></td></tr>
-                </table>
+            <div data-role="footer" data-position="fixed">
+                <div class = "ui-grid-c" >
+                    <div class="ui-block-a">
+                        <figure><img id="leftIMG" src="IMG/redLeft.png" alt="Profile Picture missing" class="swipePicture"></figure>
+                    </div>
+                    <div class="ui-block-b">
+                        <p>Swipe left to view another</p>
+                    </div>
+                    <div class="ui-block-c">
+                        <p>Swipe right to favorite</p>
+                    </div>
+                    <div class="ui-block-d">
+                        <figure><img id="rightIMG" src="IMG/greenRight.png" alt="Profile Picture missing" class="swipePicture"></figure>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
